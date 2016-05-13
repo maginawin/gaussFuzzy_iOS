3ce02e825cde5cba8e5b6d554773b3c7aff73002
wangwendong <wangwendong@WM-Mac-mini.local>
2016-05-13 09:10:20 +0800

    init

---
+++ 毛玻璃效果/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  毛玻璃效果
+//
+//  Created by User_Name_HJZ on 16/4/25.
+//  Copyright © 2016年 User_Name_HJZ_Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
